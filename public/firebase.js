ba5697b89b01e6fd6fa3e8dd679f37829f339091
ChernyshDaryna <darina.chernysh@krfk.kai.edu.ua>
2026-06-04 01:21:05 +0300

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-// 1. Добавляем импорт самой авторизации
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
 const firebaseConfig = {
@@ -14,5 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-// 2. Обязательно экспортируем настроенный auth наружу!
 export const auth = getAuth(app);
