d0b72ed7266e7b3dbf6dffe56ae2bd803f7f006a
ChernyshDaryna <darina.chernysh@krfk.kai.edu.ua>
2026-06-04 00:28:10 +0300

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+// 1. Добавляем импорт самой авторизации
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDJGuA-OndO7kZbTdyoqLm73y5fQ3TLsME",
-    authDomain: "noob-pc-57adc.firebaseapp.com",
-    projectId: "noob-pc-57adc",
-    storageBucket: "noob-pc-57adc.firebasestorage.app",
-    messagingSenderId: "818096565884",
-    appId: "1:818096565884:web:4782299539d314ea5d63f9",
-    measurementId: "G-K60DHNGBS3"
+    apiKey: "ТВОЙ_API_KEY",
+    authDomain: "ТВОЙ_PROJECT.firebaseapp.com",
+    projectId: "ТВОЙ_PROJECT_ID",
+    storageBucket: "ТВОЙ_PROJECT.appspot.com",
+    messagingSenderId: "...",
+    appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
+// 2. Обязательно экспортируем настроенный auth наружу!
+export const auth = getAuth(app);
