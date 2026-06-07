d63eadd61abc08d65186021b2b53a53452524a6e
ChernyshDaryna <darina.chernysh@krfk.kai.edu.ua>
2026-06-07 19:55:44 +0300

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,16 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "ТВОЙ_API_KEY",
-    authDomain: "ТВОЙ_PROJECT.firebaseapp.com",
-    projectId: "ТВОЙ_PROJECT_ID",
-    storageBucket: "ТВОЙ_PROJECT.appspot.com",
-    messagingSenderId: "...",
-    appId: "..."
+    apiKey: "AIzaSyDJGuA-OndO7kZbTdyoqLm73y5fQ3TLsME",
+    authDomain: "noob-pc-57adc.firebaseapp.com",
+    projectId: "noob-pc-57adc",
+    storageBucket: "noob-pc-57adc.firebasestorage.app",
+    messagingSenderId: "818096565884",
+    appId: "1:818096565884:web:4782299539d314ea5d63f9",
+    measurementId: "G-K60DHNGBS3"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
-export const auth = getAuth(app);
